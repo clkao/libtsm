f4165895ae0b09d5c11e961a84b42457600cbac7
Chia-liang Kao <clkao@clkao.org>
2013-10-30 12:00:16 +0800

    clock_gettime wrapper for __MACH__

--- src/shl_timer.h
+++ src/shl_timer.h
@@ -35,6 +35,23 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <time.h>
+#ifdef __MACH__
+#include <mach/mach_time.h>
+#define CLOCK_REALTIME 0
+#define CLOCK_MONOTONIC 0
+int clock_gettime(int clk_id, struct timespec *t){
+    mach_timebase_info_data_t timebase;
+    mach_timebase_info(&timebase);
+    uint64_t time;
+    time = mach_absolute_time();
+    double nseconds = ((double)time * (double)timebase.numer)/((double)timebase.denom);
+    double seconds = ((double)time * (double)timebase.numer)/((double)timebase.denom * 1e9);
+    t->tv_sec = seconds;
+    t->tv_nsec = nseconds;
+    return 0;
+}
+#else
+#endif
 
 struct shl_timer {
 	struct timespec start;
